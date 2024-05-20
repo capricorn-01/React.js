57c52e4b6d8d2dcd19774c0168f9c7d3880caad1
harshit <hrajwani11@gmail.com>
2024-05-20 16:38:00 +0530

    new project

---
+++ Sxc-1/src/Component/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+      <div className="footer"><p>COPYRIGHT © 2020 SIXTEEN CLOTHING CO., LTD. - DESIGN: <span className="span">TEMPLATEMO</span></p></div>
+    )
+  }
+  
+  export default Footer
